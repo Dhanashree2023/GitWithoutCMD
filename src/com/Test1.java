b2ebb54e20d32056cc9821247d8cae6d15bcaf52
Dhanashree2023 <Prachi123451@gmail.com>
2024-04-25 09:25:14 +0530

    First Commit

---
+++ src/com/Test1.java
@@ -0,0 +1,20 @@
+package com;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Test case 1");
+		System.out.println("Test case 2");
+		System.out.println("Test case 3");
+		System.out.println("Test case 4");
+		System.out.println("Test case 5");
+		
+		if (true) {
+			
+		} else {
+
+		}
+	}
+
+}
